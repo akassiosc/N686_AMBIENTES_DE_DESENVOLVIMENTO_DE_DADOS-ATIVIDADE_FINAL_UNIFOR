baa944f36c1ad71b73b0c2ca00e70d4bc4eae705
Crisostomo Frota <crisostomofrota@gmail.com>
2023-05-24 23:16:17 -0300

    commit bin/Animal.class
    Comm:   bin/Animal.class

---
+++ src/peixe.java
@@ -0,0 +1,17 @@
+public public class Peixe extends Animal {
+    public Peixe(String nome) {
+        super(nome);
+    }
+
+    public String tipoPeixe() {
+        return "Este é um peixe.";
+    }
+
+    public String alimentacaoPeixe() {
+        return "A alimentação deste peixe é desconhecida.";
+    }
+
+    public String reproducaoPeixe() {
+        return "A reprodução deste peixe é desconhecida.";
+    }
+}
---
+++ src/salmon.java
@@ -0,0 +1,19 @@
+   
+    public class Salmon extends Peixe {
+        public Salmon(String nome) {
+            super(nome);
+        }
+    
+        public String tipoSalmon() {
+            return "Este é um salmão.";
+        }
+    
+        public String caracteristicaSalmon() {
+            return "Este salmão tem características específicas.";
+        }
+        
+        public String reproducaoSalmon() {
+            return "Este salmão reproduz de maneira específica.";
+        }
+    }
+
